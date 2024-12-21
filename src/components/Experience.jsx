3e5465fc30efd61fe5f1533c8727c758c77299b0
Phoebe Lo <tsaichenlo33@gmail.com>
2024-12-21 18:20:46 -0500

    Added 3 animations

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,22 @@
 import { OrbitControls } from "@react-three/drei";
+import { useControls } from "leva";
+import { Avatar } from "./Avatar";
 
 export const Experience = () => {
+  const { animation } = useControls({
+    animation: {
+      value: "Typing",
+      options: ["Typing", "Falling", "Standing"],
+    },
+  });
+
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <group position-y={-1}>
+        <Avatar animation={animation} />
+      </group>
+      <ambientLight intensity={1.5}></ambientLight>
     </>
   );
 };
